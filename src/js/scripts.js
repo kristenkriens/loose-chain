5606f9894ed5833d1225f50ec4cda847089e5be4
Kristen Kriens <kristen.kriens@gmail.com>
2017-11-12 01:13:10 -0500

    Add new carousel markup, close mobile nav on click of item, and clean up code

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,4 +1,7 @@
 $(function() {
+  var navOffsetOrig = $('.header').offset().top;
+
+  // Smooth scroll
   $('a[href*="#"]:not([href="#"])').on('click', function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
       var target = $(this.hash);
@@ -12,19 +15,18 @@ $(function() {
     }
   });
 
-  var navOffsetOrig = $('.header').offset().top;
-
+  // Adds and removes active class to nav link on scroll
   $(window).on('scroll', function() {
     var currentOffset = $(this).scrollTop();
     var navOffset = $('.header').offset().top;
     var navHeight = $('.header').height();
 
-    if(currentOffset >= (navOffset - (navHeight / 2) + 50)) {
+    if (currentOffset >= (navOffset - (navHeight / 2) + 50)) {
       $('.header').addClass('sticky');
       $('.header + section').css('margin-top', navHeight);
     }
 
-    if(currentOffset < (navOffsetOrig - (navHeight / 2) + 50)) {
+    if (currentOffset < (navOffsetOrig - (navHeight / 2) + 50)) {
       $('.header').removeClass('sticky');
       $('.header + section').css('margin-top', 0);
     }
@@ -41,6 +43,7 @@ $(function() {
     });
   });
 
+  // Toggles mobile nav on click or hamburger
   $('.header__hamburger').on('click', function() {
     if($(window).width() < 768) {
       $('.header__nav').slideToggle();
@@ -48,64 +51,18 @@ $(function() {
     }
   });
 
-  // Slider
-  var ul = $('.workshop-reviews__carousel-inner');
-  var slide_count = ul.children().length;
-  var slide_width_pc = 100 / slide_count;
-  var slide_index = 0;
-
-  var first_slide = ul.find('li:first-child');
-  var last_slide = ul.find('li:last-child');
-
-  last_slide.clone().prependTo(ul);
-  first_slide.clone().appendTo(ul);
-
-  ul.find('li').each(function(indx) {
-    var left_percent = (slide_width_pc * indx) + '%';
-    $(this).css({'left':left_percent});
-    $(this).css({width:(100 / slide_count) + '%'});
+  // Toggles mobile nav on click of link in nav
+  $('.header__nav li a').on('click', function() {
+    if($(window).width() < 768) {
+      $('.header__nav').slideToggle();
+      $('.header__hamburger').toggleClass('header__hamburger--open');
+    }
   });
 
-  ul.css('margin-left', '-100%');
-
-  function slide(new_slide_index) {
-    var margin_left_pc = (new_slide_index * (-100) - 100) + '%';
-
-    ul.animate({'margin-left': margin_left_pc}, 500, function() {
-      if(new_slide_index < 0) {
-        ul.css('margin-left', ((slide_count) * (-100)) + '%');
-        new_slide_index = slide_count - 1;
-      } else if(new_slide_index >= slide_count) {
-        ul.css('margin-left', '-100%');
-        new_slide_index = 0;
-      }
-      slide_index = new_slide_index;
-    });
-  }
-
-  function carouselHeight() {
-    let carouselPhotoHeight = $('.workshop-reviews__photo').height() + 25;
-    let carouselParagraphsHeight = $('.workshop-reviews__text').height();
-    let carouselHeight = carouselPhotoHeight + carouselParagraphsHeight;
-
-    $('.workshop-reviews__carousel').css('height', carouselHeight);
-  }
-
-  carouselHeight();
-
-  $(window).on('resize', function() {
-    carouselHeight();
+  // Initializes the slider/carousel
+  $('.workshop-reviews__slider').slick({
+    adaptiveHeight: true,
+    autoplay: true
   });
 
-  $('.workshop-reviews__nav').on('click', function() {
-    carouselHeight();
-  });
-
-  $('.workshop-reviews__nav--left').on('click', function() {
-    slide(slide_index - 1);
-  });
-
-  $('.workshop-reviews__nav--right').on('click', function() {
-    slide(slide_index + 1);
-  });
 });
