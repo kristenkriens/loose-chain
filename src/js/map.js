95be6e70b10db9a42a045ded659ec59b42ba16ae
Kristen Kriens <kristen.kriens@gmail.com>
2017-11-12 18:28:59 -0500

    Add new marker and popup on click of marker

--- src/js/map.js
+++ src/js/map.js
@@ -1,9 +1,26 @@
 function initMap() {
+  var myStyles =[
+    {
+      featureType: 'poi',
+      elementType: 'labels',
+      stylers: [
+        {
+          visibility: 'off'
+        }
+      ]
+    }
+  ];
+
   var map = new google.maps.Map(document.getElementById('map'), {
     zoom: 16,
     disableDefaultUI: true,
     scrollwheel: false,
-    center: {lat: 43.6483, lng: -79.3979}
+    center: {lat: 43.6483, lng: -79.3979},
+    styles: myStyles
+  });
+
+  var infowindow = new google.maps.InfoWindow({
+    content: '<p><strong>Loose Chain Bike Co.</strong></p><p>483 Queen Street West</p><p>Toronto, ON M5V 2A9</p>'
   });
 
   var image = 'dist/images/marker.png';
@@ -13,14 +30,21 @@ function initMap() {
     icon: image
   });
 
+  marker.addListener('click', function() {
+    infowindow.open(map, marker);
+  });
+
   var center;
-    function calculateCenter() {
-      center = map.getCenter();
-    }
-    google.maps.event.addDomListener(map, 'idle', function() {
-      calculateCenter();
-    });
-    google.maps.event.addDomListener(window, 'resize', function() {
-      map.setCenter(center);
-    });
+
+  function calculateCenter() {
+    center = map.getCenter();
+  }
+
+  google.maps.event.addDomListener(map, 'idle', function() {
+    calculateCenter();
+  });
+
+  google.maps.event.addDomListener(window, 'resize', function() {
+    map.setCenter(center);
+  });
 };
